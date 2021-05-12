a2746bca3ada341bb29a8231f4da7e8e62182a58
Ambrohkh <ambrokhstepan0906@gmail.com>
2021-05-12 05:17:27 +0300

    js

---
+++ my.js
@@ -0,0 +1,53 @@
+/*
+function task2(metres){
+    const kilometres = metres / 1000;
+    return `${kilometres.toFixed(2)} км.`;
+}
+
+function conwert(n) {
+    const grn = n / 28;
+    return `${grn.toFixed(2)} USD.`;
+}//бля нарешті я зрозумів)
+
+function name(a,b,c) {
+    return Math.min(a,b,c);
+}
+
+
+       function name(str) {
+           arr = str.split('');
+           arr.reverse();
+           return `${arr=arr.join('')}`;
+       }
+
+       function staircase(n) {
+        for (let i = 0; i < n; i++) {
+            let step = ' ';
+      
+         for (let j = 0; j < n; j++) {
+            if (j <= i) {
+              step += '#'
+           } else {
+              step += ' ';
+           }
+         }
+         console.log(step)
+        }
+      }
+
+      function cursoreCheck(str) {
+         const check = str.toLowerCase();
+         if (check.indexOf('iron') != -1 
+         || check.indexOf('cursor') != -1 
+         || check.indexOf('ostap') != -1) {   
+        return true;
+         } 
+        return false;
+      }
+      function upperCase(str) {
+         const upper = str.toUpperCase();
+          return upper;
+      }
+      */// УРОК 7
+    // УРОК 8
+    let frferf =
